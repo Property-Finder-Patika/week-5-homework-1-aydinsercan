04e92cc41b42955c3d9a7ce5f24b583633254597
aydinsercan <aydinser17@itu.edu.tr>
2022-08-01 00:13:47 +0300

    license mech. updated

---
+++ License Mechanism/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+//The Proxy pattern is a structural pattern whose purpose is to provide a surrogate or intermediary of an object to control its access.
+//Usually wraps an object to hide some of its characteristics.
+
+type Provider interface {
+	Provide()
+}
+
+type Software struct{}
+
+func (c *Software) Provide() {
+	fmt.Println("Not reached to limit")
+}
+
+type User struct {
+	Num0fUsers int
+}
+
+type SoftwareProxy struct {
+	soft Software
+	user *User
+}
+
+func (c *SoftwareProxy) Provide() {
+	if c.user.Num0fUsers <= 32 {
+		c.soft.Provide()
+	} else {
+		fmt.Println("Concurrent number of users reached the limit for this software")
+	}
+}
+
+func NewCarProxy(user *User) *SoftwareProxy {
+	return &SoftwareProxy{Software{}, user}
+}
+
+func main() {
+	soft := NewCarProxy(&User{67})
+	soft.Provide()
+}
